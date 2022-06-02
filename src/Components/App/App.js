a07133a1a88e656afce9aecda3f491c5f6f70e3e
Saves <saves1484@gmail.com>
2022-06-02 13:44:55 -0700

    add removeTrack and updatePlaylistName functions to App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -18,6 +18,8 @@ class App extends React.Component {
       {name: 'playlistname3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6}]
     }
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -30,6 +32,16 @@ class App extends React.Component {
 
   }
 
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+    this.setState({ playlistTracks: tracks });
+  }
+
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
+  }
+
   render() {
     return (
       <div>
@@ -40,7 +52,9 @@ class App extends React.Component {
         <SearchResults searchResults={this.state.searchResults}
                        onAdd={this.addTrack} />
         <Playlist playlistName={this.state.playlistName}
-                  playlistTracks={this.state.playlistTracks} />
+                  playlistTracks={this.state.playlistTracks}
+                  onRemove={this.removeTrack}
+                  onNameChange={this.updatePlaylistName} />
     </div>
   </div>
 </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,12 +2,28 @@ import React from 'react';
 import './Track.css';
 
 class Track extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.addTrack = this.addTrack.bind(this);
+        this.removeTrack = this.removeTrack.bind(this);
+    }
     renderAction() {
         if (this.props.isRemoval) {
-            return <button className="Track-action">-</button>
+            return <button onClick={this.removeTrack}
+                           className="Track-action">-</button>
         } else {
-            return <button className="Track-action">+</button>
+            return <button onClick={this.addTrack} 
+                           className="Track-action">+</button>
         }
+    }
+
+    addTrack() {
+        this.props.onAdd(this.props.track);
+    }
+
+    removeTrack() {
+        this.props.onRemove(this.props.track);
     }
 
     render() {
