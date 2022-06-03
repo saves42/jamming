55138b6ef2f42a2aeda0229b731c2bc55065c099
Saves <saves1484@gmail.com>
2022-06-03 13:59:26 -0700

    add spotify.js module

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -5,6 +5,10 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      term: ''
+    }
+
     this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
   }
@@ -20,7 +24,7 @@ class SearchBar extends React.Component {
   render() {
       return (
           <div className="SearchBar">
-            <input placeholder="Enter A Song, Album, or Artist" />
+            <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
             <button className="SearchButton">SEARCH</button>
           </div>
                   )
